efd3649f7eca20d42436ae2f3710a72793a990c4
Nisarg Limbachiya <nghosty07@gmail.com>
2024-12-29 13:13:26 +0530

    Admin - base routes and some file created

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import AdminDesh from './components/AdminDesh';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route>
+          <Route path='/' Component={AdminDesh} />
+          <Route path="*" Component={NotFound}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+   </>
   );
 }
 
