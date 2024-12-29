ce31576908452ee53cff9d25a31a5ddb5f8aef99
rushabh07 <rushabhsaraiya661@gmail.com>
2024-12-29 19:13:52 +0530

    some changes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div classname="font-bold">Home Page for PC WORLD dfhsdifhdfhsdfjsdfhsdfhsdjkfh</div>
+    <div classname="font-bold">Home Page for PC WORLD rushabh</div>
   )
 }
 
