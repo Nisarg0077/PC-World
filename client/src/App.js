0d3e92754a9eca2b24005a2741e18d4336c5dc4c
Nisarg Limbachiya <nghosty07@gmail.com>
2024-12-29 12:37:56 +0530

    new files added

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex flex-col justify-center align-middle items-center'>
+      <h1 className='text-violet-500 text-4xl font-bold'>This is an Sample page............</h1>
+      <h1 className='text-violet-500 text-4xl font-bold'>This is an Sample page............</h1>
+    </div>
+  );
+}
+
+export default App;
