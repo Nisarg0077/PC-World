ba0b443e9751196a222defb608f1c8a5b9fd4215
Darshpatel2004 <darshpatel4002@gmail.com>
2025-01-18 16:07:16 +0530

    setup done

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center align-middle">
       <h1 className="font-bold text-blue-500 text-4xl ">Hello from Kush</h1>
+      <h1 className="font-bold text-green-500 text-4xl ">Hello from Darsh</h1>
+      ksnlkfsjlsjljlfjslfjslkf
     </div>
   );
 }
