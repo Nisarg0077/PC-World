11192f52b7ae45d67a0fd69f7fbe581b66506cff
Nisarg Limbachiya <143074221+Nisarg0077@users.noreply.github.com>
2025-01-29 16:45:34 +0530

    some client site changed.
    Client:
    
    -New folders Created like components, images, view.
    -New Home Page and Shop Now Page created.
    -New Navbar cerated.
    -some style chganges.
    
    Backend:
    
    -/api/products Modified to load images images

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from './view/NotFound';
+import Home from './view/Home';
+import ShopNow from './view/ShopNow';
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center align-middle">
-      <h1 className="font-bold text-blue-500 text-4xl ">Hello from Kush</h1>
-      <h1 className="font-bold text-green-500 text-4xl ">Hello from Darsh</h1>
-      ksnlkfsjlsjljlfjslfjslkf
-    </div>
+    <>
+      <>
+    <BrowserRouter>
+
+      <Routes>
+       
+      <Route path='/' Component={Home} />
+      {/* <Route path='/login' Component={Login} />
+      <Route path='/logout' Component={Logout} />
+      <Route path='/register' Component={Register} /> */}
+      <Route path="/shopnow" Component={ShopNow}/>
+      <Route path="*" Component={NotFound}/>
+      </Routes>
+    </BrowserRouter>
+    </>
+    </>
   );
 }
 
